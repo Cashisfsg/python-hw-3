cbfcf67a24ffe333116c763b779c354210fa0a91
Cashisfsg <cashisfsg@gmail.com>
2022-07-29 16:50:58 +0300

    Fix it

--- double_zero.py
+++ double_zero.py
@@ -1,30 +1,15 @@
-def double_zero(amount):
-    try:
-        amount = int(amount)
-    except ValueError:
-        print('Amount must be a number')
-        return
-
-    if amount < 1:
-        print('List must contain at least 1 element')
-        return
-
-    try:
-        list = [int(input(f'Enter number #{i + 1}: ')) for i in range(amount)]
-    except ValueError:
-        print('Invalid value')
-        return
+def double_zero(lst):
 
     double_zero_list = []
 
-    for i in range(len(list)):
-        if list[i] == 0:
-            double_zero_list += [list[i]] * 2
+    for i in range(len(lst)):
+        if lst[i] == 0:
+            double_zero_list += [lst[i]] * 2
         else:
-            double_zero_list += [list[i]]
+            double_zero_list += [lst[i]]
 
-    print(f'Original list: {list} -> Double zero list: {double_zero_list}')
+    print(f'Original list: {lst} -> Double zero list: {double_zero_list}')
     return double_zero_list
 
 if __name__ == '__main__':
-    double_zero(input('Enter amount of numbers: '))
+    double_zero([0, 0])
--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,29 +1,13 @@
-def remove_duplicates(amount):
-
-    try:
-        amount = int(amount)
-    except ValueError:
-        print('Amount must be a number')
-        return
-
-    if amount < 1:
-        print('List must contain at least 1 element')
-        return
-
-    try:
-        list = sorted([int(input(f'Enter number #{i + 1}: ')) for i in range(amount)])
-    except ValueError:
-        print('Invalid value')
-        return
+def remove_duplicates(lst):
 
     filtered_list = []
 
-    for i in list:
+    for i in lst:
         if i not in filtered_list:
             filtered_list.append(i)
 
-    print(f'Sorted list: {list} -> Filtered list: {filtered_list}')
+    print(f'Sorted list: {lst} -> Filtered list: {filtered_list}')
     return filtered_list
 
 if __name__ =='__main__':
-    remove_duplicates(input('Enter amount of numbers: '))
+    remove_duplicates([1, 1, 1, 2, 3, 3, 4])
